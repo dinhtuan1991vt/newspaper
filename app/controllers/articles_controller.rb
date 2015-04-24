9906d1b093eb1841a64d0c88acd6f998883a8111
Nguyen tuan <dinhtuan1991vt2013@gmail.com>
2015-04-24 11:54:50 +0700

    Add create article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,16 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+  end
+
+  def create
+    author_id = params[:article][:author_id]
+    if !author_id.empty?
+      author = Author.find(author_id)
+      article = author.articles.create(params.require(:article).permit(:name, :text))
+    end
+    redirect_to articles_path
+  end
 end
