acd3c180643a7dc1adca692f0dc7bb08721f362b
Nguyen tuan <dinhtuan1991vt2013@gmail.com>
2015-05-04 17:19:31 +0700

    Update

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,8 @@ class Article < ActiveRecord::Base
   has_many :comments, dependent: :nullify
   validates :name, length: { maximum: 255 }
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => ""
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  validates_attachment_file_name :avatar, :matches => [/png\Z/, /jpe?g\Z/]
 
   def avatar_url
       avatar.url(:thumb)
